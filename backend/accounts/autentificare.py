dd46fc5c5d1cfa8ff0b16e6eaa6372594bf8b6b7
teobaila1 <teodormihai.baila@ulbsibiu.ro>
2026-01-20 01:18:44 +0200

    Changed the logic of the database.

--- backend/accounts/autentificare.py
+++ backend/accounts/autentificare.py
@@ -1,11 +1,16 @@
+import os
+
 import jwt
 import datetime
 import json
 from flask import Blueprint, request, jsonify
-from backend.config import get_conn, SECRET_KEY
+from backend.config import get_conn
 from backend.passwords.security import check_password
 
 autentificare_bp = Blueprint('autentificare', __name__)
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "cheie_super_secreta_hwarang_2026")
 
 
 @autentificare_bp.post('/api/login')
